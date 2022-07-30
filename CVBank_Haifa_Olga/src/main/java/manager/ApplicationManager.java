a203ec95450fd78a3f4cf9d7295b9619ffb566b4
olacola06 <olamarchen@gmail.com>
2022-07-30 20:18:44 +0300

    30 July

---
+++ CVBank_Haifa_Olga/src/main/java/manager/ApplicationManager.java
@@ -0,0 +1,33 @@
+package manager;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class ApplicationManager {
+    String browser;
+    EventFiringWebDriver wd;
+    public ApplicationManager(String browser) {
+        this.browser = browser;
+    }
+    public void init() {
+        if (browser.equals(BrowserType.CHROME)) {
+            wd = new EventFiringWebDriver(new ChromeDriver());
+        }else if(browser.equals(BrowserType.FIREFOX)){
+            wd = new EventFiringWebDriver(new FirefoxDriver());
+        }else if(browser.equals(BrowserType.EDGE)){
+            wd = new EventFiringWebDriver(new EdgeDriver());
+        }
+        wd.manage().window().maximize();
+        wd.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        wd.navigate().to("https://cv-bank-new.web.app/");
+
+    }
+    public void stop(){
+        wd.quit();
+    }
+}
---
+++ CVBank_Haifa_Olga/src/test/java/tests/TestBase.java
@@ -0,0 +1,38 @@
+package tests;
+
+import manager.ApplicationManager;
+import org.openqa.selenium.remote.BrowserType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.Type;
+
+public class TestBase {
+
+    protected static ApplicationManager app = new ApplicationManager
+            (System.getProperty("browser", BrowserType.CHROME));
+    Logger logger = LoggerFactory.getLogger(TestBase.class);
+
+    @BeforeMethod
+    public void methodStart(Method m){
+        logger.info("start to test method ->>"+m.getName());
+    }
+    @AfterMethod
+    public void methodFinished(Method m){
+        logger.info("end to test method ->>"+m.getName());
+    }
+    @BeforeSuite
+    public void start(){
+        app.init();
+    }
+    @AfterSuite
+    public void end(){
+        app.stop();
+    }
+
+}
